27f5665ba219e49acb3b08fed96dfe3a626c5e4b
ronapelbaum <ronapelbaum@gmail.com>
2018-06-27 09:45:40 +0300

    add the require problem

--- src/closure/app.js
+++ src/closure/app.js
@@ -3,6 +3,11 @@ goog.require('my.b');
 (function () {
     console.log('app:closure');
     my.b();
+
+    // see the "require problem"
+    // my.a()
+
+    // see the "global problem"
     // console.log('my', my);
     // console.log('global.my', global.my);
 })();
