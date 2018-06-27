52abc64e1c8ffd9a7a63d752cc588b8567d73937
ronapelbaum <ronapelbaum@gmail.com>
2018-06-27 09:11:14 +0300

    phase 1 - no global

--- src/closure/a.js
+++ src/closure/a.js
@@ -4,4 +4,4 @@ my.a = function () {
     console.log('my.a');
 };
 
-global.my = global.my || my;
+// global.my = global.my || my;
--- src/closure/app.js
+++ src/closure/app.js
@@ -3,6 +3,6 @@ goog.require('my.b');
 (function () {
     console.log('app:closure');
     my.b();
-    console.log('my', my);
-    console.log('global.my', global.my);
+    // console.log('my', my);
+    // console.log('global.my', global.my);
 })();
